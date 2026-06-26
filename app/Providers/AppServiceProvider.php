c40a1afd7fa2b52b4ba3ffd4c52ea30f7b63161b
Faiilo <fluffylope@mail.ru>
2026-06-26 20:42:00 +0300

    Force HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;  // <-- ЭТА СТРОЧКА НОВАЯ!
 use App\Models\User;
 use Illuminate\Pagination\Paginator;
 
@@ -16,6 +17,11 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        // ПРИНУДИТЕЛЬНО ИСПОЛЬЗУЕМ HTTPS НА ПРОДАКШЕНЕ
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+
         Gate::define('admin', function (User $user) {
             return $user->isAdmin();
         });
